f87688d258f7507f12e3c738bdabff34dcd3e292
olafurv15 <olafurv15@ru.is>
2016-10-12 17:14:48 +0000

    test fyrir 1 tolu

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,8 +3,11 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		//if(text.equals("")){
+		if(text.equals("")){
 			return 0;
-		//}
+		}
+		else {
+			return 1;
+		}
 	}
 }
