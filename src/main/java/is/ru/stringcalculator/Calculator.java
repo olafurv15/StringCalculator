22751bd0e6218e4e33195f131e4ec5607c982a9d
olafurv15 <olafurv15@ru.is>
2016-10-12 17:31:19 +0000

    Test fyrir margar tolur baett vid

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,8 +7,7 @@ public class Calculator {
 			return 0;
 		}
 		else if(text.contains(",")){
-			String [] numbers = text.split(",");
-			return toInt(numbers[0]) + toInt(numbers[1]);
+			return sum(splitNumbers(text));
 		}
 		else
 			return 1;
@@ -16,5 +15,19 @@ public class Calculator {
 
 	private static int toInt(String text) {
 		return Integer.parseInt(text);
+	}
+
+	private static String[] splitNumbers(String numbers) {
+		return numbers.split(",");
+	}
+
+	private static int sum(String[] numbers){
+		int total = 0;
+
+		for(String number : numbers) {
+			total += toInt(number);
+		}
+
+		return total;
 	}
 }
