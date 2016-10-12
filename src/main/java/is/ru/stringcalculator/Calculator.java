9f0411b596ad6537577103bd88b6ba71d8ec29d3
olafurv15 <olafurv15@ru.is>
2016-10-12 17:23:14 +0000

    Test fyrir 2 tolur

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,8 +6,15 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else {
-			return 1;
+		else if(text.contains(",")){
+			String [] numbers = text.split(",");
+			return toInt(numbers[0]) + toInt(numbers[1]);
 		}
+		else
+			return 1;
+	}
+
+	private static int toInt(String text) {
+		return Integer.parseInt(text);
 	}
 }
